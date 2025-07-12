ee8249b81d7a2142b78362aba2d2c642c91e6057
Vukosi <vukosimohlabini16@gmail.com>
2025-07-12 12:56:02 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+
+   <>
+    <div className='frontpageHero'></div>
+    <div></div>
+    <div></div>
+   </>
+  
+)
+}
+
+export default App
