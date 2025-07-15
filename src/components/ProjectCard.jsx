2c7b9e2943854b90310ac6a2d64c77e958f94475
Vukosi <vukosimohlabini16@gmail.com>
2025-07-15 14:51:14 +0200

    Projects page commplete

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,21 @@
+import '../styles/Projects.css'
+
+const ProjectCard = ({projName, projDesc, projImage, projSvg}) => {
+
+
+  return (
+
+    <>
+        <a href={`https://github.com/Vukosi16/${projName}`} target="_blank" rel="noopener noreferrer">
+                <div className="pCard" style={{backgroundImage: `url(${projImage})`}}>
+                <img className='projSvg' src={projSvg} alt="" />
+                <h2 className='projName'>{projName}</h2>
+                <p className='projDesc'>{projDesc}</p>
+            </div>
+        </a>
+
+    </>
+  )
+}
+
+export default ProjectCard
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,68 @@
-import React from 'react'
+import '../styles/Projects.css'
+import PageNav from '../components/PageNav'
+import ProjectCard from '../components/ProjectCard'
+import projBackground from '../assets/pCardBackground.avif'
+import portfolioIcon from '../assets/portfolioIcon.svg'
+import gameIcon from '../assets/gameIcon.svg'
+import calculatorIcon from '../assets/calculatorIcon.svg'
+import youtubeIcon from '../assets/youtubeIcon.svg'
 
 const Projects = () => {
   return (
-    <div>
-      projects
-    </div>
+    <>
+      <div className="mainContainerProjects">
+        <PageNav/>
+        <div className="projectsGridContainer">
+          <h1 className='projectsHeadingText'>Projects</h1>
+          <div className="projectGrid">
+
+              <ProjectCard 
+                projSvg={portfolioIcon} 
+                projName={"Portfolio"} 
+                projDesc={"This is a portfolio made using React. It’s a simple, responsive design built with basic components like a navbar, project cards, and a contact section to showcase different projects in a clean and interactive way."}
+                projImage={projBackground}
+              />
+
+              <ProjectCard 
+                projSvg={gameIcon} 
+                projName={"Etch-A-Sketch"} 
+                projDesc={"This is a browser-based Etch A Sketch project made with HTML, CSS, and JavaScript. It features a grid that users can draw on by hovering, with simple controls for changing colors and clearing the board"}
+                projImage={projBackground}
+              />
+
+              <ProjectCard 
+                projSvg={gameIcon} 
+                projName={"Battleship"} 
+                projDesc={"This is a Battleship game built with JavaScript using object-oriented programming. It features classes to handle the game board, ships, and turns for an interactive gameplay experience."}
+                projImage={projBackground}
+              />
+
+              <ProjectCard 
+                projSvg={gameIcon} 
+                projName={"Rock-Paper-Scissors"} 
+                projDesc={"This is a simple Rock Paper Scissors game created with JavaScript, HTML, and CSS. It lets users play against the computer with instant results and keeps track of the score."}
+                projImage={projBackground}
+              />
+
+              <ProjectCard 
+                projSvg={calculatorIcon} 
+                projName={"Calculator"} 
+                projDesc={"This is a basic calculator built using HTML, CSS, and JavaScript. It performs standard arithmetic operations with a clean, user-friendly interface."}
+                projImage={projBackground}
+              />
+
+              <ProjectCard 
+                projSvg={youtubeIcon} 
+                projName={"Youtube-Clone"} 
+                projDesc={"This is a YouTube clone created using HTML and CSS. It mimics the layout and style of YouTube’s homepage, featuring video thumbnails, navigation, and a responsive design."}
+                projImage={projBackground}
+              />
+            
+          </div>
+        </div>
+      </div>
+    
+    </>
   )
 }
 
