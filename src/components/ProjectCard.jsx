0fd8a0f13990d6a80400d795f05f0a82b7bfcd7a
Vukosi <vukosimohlabini16@gmail.com>
2025-07-22 15:56:55 +0200

    contact page complete

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -7,7 +7,7 @@ const ProjectCard = ({projName, projDesc, projImage, projSvg}) => {
 
     <>
         <a href={`https://github.com/Vukosi16/${projName}`} target="_blank" rel="noopener noreferrer">
-                <div className="pCard" style={{backgroundImage: `url(${projImage})`}}>
+              <div className="pCard" style={{backgroundImage: `url(${projImage})`}}>
                 <img className='projSvg' src={projSvg} alt="" />
                 <h2 className='projName'>{projName}</h2>
                 <p className='projDesc'>{projDesc}</p>
--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import '../styles/contact.css'
+import PageNav from '../components/PageNav'
+import phoneSvg from '../assets/phoneIcon.svg'
+import linkedInIcon from '../assets/linkedInIcon.svg'
+import gmailIcon from '../assets/emailIcon.svg'
+import githubIcon from '../assets/githubIcon.svg'
 
 const ContactMe = () => {
-  console.log(2003)
   return (
-    <div>
-      Contact Me
-    </div>
+    <>
+      <div className="contactPageContainer">
+        <PageNav/>
+
+        <div className="contactContainer">
+          <div className="heroTextContainer">
+            <h1 style={{fontSize: '3em'}}>Contact Me</h1>
+            <p style={{fontSize: '1.5em'}}>For more information about where to get a hold of me or view my work and experience look below.</p>
+          </div>
+
+          <div className="linkToContactContainer">
+
+            <div className="contactMethodContainer">
+              <img className='linkIcons' src={phoneSvg} alt="Phone Svg" />
+              <p style={{fontSize: '1.1em'}}>Phone Number: +27 67 257 5232</p>
+            </div>
+            <div className="contactMethodContainer">
+              <a className="links" href="https://www.linkedin.com/in/vukosi-mohlabini-158646174/">
+                <img className='linkIcons inLinkIcon' src={linkedInIcon} alt="LinkedIn Svg" />
+              </a>
+              <p style={{fontSize: '1.1em'}}>LinkedIn Profile</p>
+            </div>
+            <div className="contactMethodContainer">
+                <img className='linkIcons' src={gmailIcon} alt="Email Svg" />
+                <p style={{fontSize: '1.1em'}}>Gmail: Vukosimohlabini16@gmail.com</p>
+            </div>
+            <div className="contactMethodContainer">
+              <a className="links" href="https://github.com/Vukosi16">
+                <img className='linkIcons inLinkIcon' src={githubIcon} alt="Github Svg" />
+              </a>
+              <p style={{fontSize: '1.1em'}}>Github Profile</p>
+            </div>
+            
+          </div>
+        </div>
+
+      </div>
+    </>
   )
 }
 
